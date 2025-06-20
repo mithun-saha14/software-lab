32706b2dad9a965c491573b2a06298efffc7f9d7
Mithun Saha <mithunsaha1453@gmail.com>
2025-06-20 14:52:42 +0600

    Initial Commit

---
+++ sum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SumOfFiveNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+
+        System.out.println("Enter 5 numbers:");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Number " + i + ": ");
+            int num = scanner.nextInt();
+            sum += num;
+        }
+
+        System.out.println("The sum of the 5 numbers is: " + sum);
+    }
+}
